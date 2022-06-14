2a1cc8e3be770bf877c28bab5f9ce5f9247f58b2
dejan <dejan@meznarc.si>
2022-06-14 20:21:26 +0200

    do not use strings

--- src/metrics/PowerSupply.php
+++ src/metrics/PowerSupply.php
@@ -15,9 +15,10 @@ class PowerSupply implements MetricInterface
     public function printPrometheus()
     {
         echo "hpe_psu_present{number=\"$this->number\"} " . (int)($this->present) . "\n";
-        echo "hpe_psu_redundant{number=\"$this->number\"} " . (int)($this->redundant??-1) . "\n";
-        echo "hpe_psu_condition{number=\"$this->number\"} \"" . ($this->condition??"unknown") . "\"\n";
-        echo "hpe_psu_hotplug{number=\"$this->number\"} " . (int)($this->hotplug??-1) . "\n";
+        echo "hpe_psu_redundant{number=\"$this->number\"} " . (int)($this->redundant ?? -1) . "\n";
+        echo "hpe_psu_condition{number=\"$this->number\", value=\"" . ($this->condition ?? "unknown")
+            . "\"} \"" . (int)(($this->condition ?? "NaN") === "ok") . "\"\n";
+        echo "hpe_psu_hotplug{number=\"$this->number\"} " . (int)($this->hotplug ?? -1) . "\n";
         echo "hpe_psu_power{number=\"$this->number\"} " . ($this->power ?? -1.0) . "\n";
     }
 
